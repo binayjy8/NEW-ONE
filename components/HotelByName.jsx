48b2d85b49167aeb4fa1da68574b002e1b9b763a
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-10-14 22:58:09 +0530

    file connect with backend

--- components/HotelByName.jsx
+++ components/HotelByName.jsx
@@ -2,6 +2,7 @@ import useFetch from "../useFetch";
 
 const HotelByName = ({name}) => {
     const { data, loading, error } = useFetch(`https://hotel-6q8k.vercel.app/hotels${name}`);
+    console.log(data);
 };
 
 export default HotelByName;
