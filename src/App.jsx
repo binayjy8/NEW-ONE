7609cb3f31ad0f2d149ed06fb6dc29ef24ecc20a
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-10-17 22:30:02 +0530

    form data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hotels from "../components/Hotel";
 import HotelByName from "../components/HotelByName";
+import AddMovieForm from "../components/AddMovieForm";
 import useFetch from "../useFetch";
 
 export default function App() {
