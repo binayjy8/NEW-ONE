5eb5c75df82d662916c814d9fb9aeab3ddb61ff3
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-10-17 20:01:47 +0530

    file

--- components/HotelByName.jsx
+++ components/HotelByName.jsx
@@ -10,6 +10,7 @@ const HotelByName = ({name}) => {
             <p>Category: {data.category}</p>
             <p>Location: {data.location}</p>
             <p>Rating: {data.rating}</p>
+            <p>PriceRange: {data.priceRange}</p>
         </div>
     ) : (
         loading && <p>Loading...</p>
