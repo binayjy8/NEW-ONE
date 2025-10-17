2d37f5697dffe34f40bbcae9e2487d9865eb04c7
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-10-17 22:28:08 +0530

    form data

--- components/AddMovieForm.jsx
+++ components/AddMovieForm.jsx
@@ -36,7 +36,7 @@ const AddMovieForm = () => {
                     name="title"
                     value={formData.title}
                     onChange={handleChange}
-                />
+                />{" "}
             </form>
         </div>
     )
