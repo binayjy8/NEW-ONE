607538c827e98425be1b63272695e9a301bab15a
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-10-19 21:09:26 +0530

    form data

--- components/Hotel.jsx
+++ components/Hotel.jsx
@@ -3,7 +3,7 @@ import useFetch from "../useFetch";
 const Hotels = () => {
 
     const { data, loading, error } = useFetch("https://hotel-6q8k.vercel.app/hotels");
-    console.log(data);
+
 
     return (
         <div>
