2e2ea558a120de23280904c3631d036e52ae501c
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-10-21 13:32:15 +0530

    form data

--- components/Hotel.jsx
+++ components/Hotel.jsx
@@ -4,9 +4,9 @@ const Hotels = () => {
 
     const { data, loading, error } = useFetch("https://hotel-6q8k.vercel.app/hotels");
 
-    const handleDelete = async () => {
+    const handleDelete = async (hotelId) => {
         try{
-
+            const response = await fetch(`https://hotel-6q8k.vercel.app/hotels/${hotelId}`)
         }catch(error){
             console.log(error);
         }
